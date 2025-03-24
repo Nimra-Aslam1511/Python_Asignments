f7be2f764434c34553748d6f965261c12bdc62ea
Nimra-Aslam1511 <nimraaslam8925@gmail.com>
2025-03-24 21:16:54 +0500

    Add files via upload
    Here is the assignment of Topic1.2: Expressions in Python.

---
+++ Assignment(Ch#1,1.2.py
@@ -0,0 +1,41 @@
+#1...Create an expression that multplies two numbers and stores the result in a variable.
+
+num1= 5
+num2 = 4
+
+multiply = num1 * num2
+
+print(f" The multiplication of {num1} and {num2} is {multiply}.")
+
+######################################################################################
+#2... Combine a string variable with another string using concatenation.
+
+First_name = "Nimra"
+Last_name = "Aslam"
+
+Full_name = First_name + " " +Last_name
+print(Full_name)
+
+######################################################################
+#3....Use an expression to calculate the area of a rectangle .
+
+Length = 15
+Width  = 10
+Area = Length * Width
+
+print(Area)
+
+########################################################################
+#4... Evaluate the expression(10+5)*2-3 and print the result.
+
+result = (10 + 5) *2 - 3
+print(result)
+
+######################################################################
+#5... Write an expression that converts temperature from Celsius to Farenheit using formula.
+
+C= input("Enter the temperature in Celsius")
+F = int(C) *(int(9/5)) +32
+
+print(f" {C} degree Celsius in Farenheit is {F}")
+
