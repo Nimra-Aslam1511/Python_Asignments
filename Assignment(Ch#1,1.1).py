7db7dc73552bbdd67a32ff9db3df638416a06eb0
Nimra-Aslam1511 <nimraaslam8925@gmail.com>
2025-03-24 21:15:23 +0500

    Add files via upload
    Here is the assignment of Topic 1.1: Intro. to variables in python.

---
+++ Assignment(Ch#1,1.1).py
@@ -0,0 +1,44 @@
+
+#1....Create Variables to store your name,age, and favourite color.
+
+Name = "Nimra"
+Age = 25
+Fav_color = "Black"
+print (f"My name is {Name}, my age is {Age}, and my favourite color is {Fav_color}.")
+#......................................................................
+
+#2....Assign interger and floating- point values to two different variables and print them.
+ 
+Integer = 12
+Float = 2.5
+print(f"{Integer} is integer value and {Float} ia a floating point value.")
+#.............................................................................
+
+#3...Swap two variables without using a third variable.
+
+x =5
+y = 8
+print(f"Before swapping, the value of x is {x} and y is {y}")
+x,y = y,x
+print(f"After swapping, the value of x is {x} and y is {y}")
+#...................................................................
+
+#4...Create a variable to store the value of Pi(3.14159)and print it.
+
+Pi = 3.14159
+print(f"The value of Pi is {Pi}.")
+#.....................................................................
+
+#5......Assign a boolean value to a variable and print its type usng type().
+
+name = True
+print(type(name))
+#..............................................
+
+#6....Type conversion needs to be added in notes..
+
+First_num = 14
+Second_num = "34"
+
+Sum = First_num + int(Second_num)
+print(Sum)
